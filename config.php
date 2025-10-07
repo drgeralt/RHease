b178209f5b6fd650ff618901359e370efbb7b75a
coisacruel <vitorialealsantos06@gmail.com>
2025-10-07 12:04:34 -0300

    mudando database q tava com usuario diferente

--- config.php
+++ config.php
@@ -19,15 +19,6 @@ if (!defined('DB_NAME')) {
 	define('DB_NAME', getenv('DB_NAME') ?: 'rhease');
 }
 
-require_once __DIR__ . '/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-// Configurações do Banco de Dados
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'rhease');
 
 // Configuração da URL base
 if (!defined('BASE_URL')) {
